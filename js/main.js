11b7f9b1537de87d9cd2b6e7d5ed7f1fef0bc471
mathesbh <mathesbh@gmail.com>
2020-09-04 10:49:17 -0300

    Function add Users

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
-function helloWorld() {
-    for (let i = 3; i > 0; i--) { console.log(i); }
-    console.log('...');
-    console.log('Hello World!');
+function addUser() {
+    if (localStorage.score) {
+        localStorage.score = Number(localStorage.score) + 1
+    } else {
+        localStorage.score = 1
+    }
+
+    const inputName = document.getElementById('name').value
+    const inputEmail = document.getElementById('email').value
+
+    const user = JSON.stringify({
+        Nome: inputName,
+        Email: inputEmail,
+    })
+
+
+    localStorage.setItem('user_' + localStorage.score, user)
+
 }
-helloWorld();
